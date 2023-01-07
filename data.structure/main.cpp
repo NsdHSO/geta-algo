6fb4735b89a26f9e9de14471ba0bd369a279f4cb
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2023-01-07 07:28:49 +0200

    Initial Commit

---
+++ data.structure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data.structure
+//
+//  Created by David Nechiforel on 07.01.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
