767882f63e68ef0779c3e49120f007a4b0014097
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2023-01-08 05:03:47 +0200

    feat:

--- data.structure/main.cpp
+++ data.structure/main.cpp
@@ -6,6 +6,18 @@
 //
 
 #include <iostream>
+using namespace std;
+void swap(int *p, int *b){
+    int temp;
+    temp = *p;
+    *p = *b;
+    *b = temp;
+}
 
 int main(int argc, const char * argv[]) {
+    int a=10,b=20;
+    
+    swap(&a, &b);
+    cout<< a<< endl;
+    cout<< b<<endl;
 }
