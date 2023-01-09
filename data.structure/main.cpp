225f4fadaaa5bb925a67352b94fd0d4241a48e83
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2023-01-09 07:26:31 +0200

    test: added struct parameter at address

--- data.structure/main.cpp
+++ data.structure/main.cpp
@@ -6,18 +6,47 @@
 //
 
 #include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
 using namespace std;
-void swap(int *p, int *b){
+void swap(int &p, int &b){
     int temp;
-    temp = *p;
-    *p = *b;
-    *b = temp;
+    temp = p;
+    p = b;
+    b = temp;
 }
+
+struct Student{
+    char firstName[25];
+    char lastName[25];
+    char address[50];
+};
+
+int * newArray(int length){
+    int *p = (int *)malloc(sizeof(int) * length);
+    return p;
+}
+
+ void name(struct Student *student){
+     student->lastName[0]='t';
+    
+
+    
+ };
 
 int main(int argc, const char * argv[]) {
     int a=10,b=20;
-    
-    swap(&a, &b);
+    int *p;
+    p = newArray(10);
+    p[2] = 20;
+    cout << p[2]<<endl;
+    swap(a, b);
     cout<< a<< endl;
     cout<< b<<endl;
+    
+    struct Student std;
+    name(&std);
+    cout<<std.lastName<<endl;
+    
 }
